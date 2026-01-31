795cbdc76f86ab275c1e8bbd9296abec368ded2f
Legende_7 <konfusa.wesperra@googlemail.com>
2026-01-31 16:15:54 +0100

    clean up & switched out the publish workflow

--- run-example.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")"
-
-.venv/bin/python src/example.py
-
-rm -rf src/boxes_tui/__pycache__
-rm -rf src/boxes_tui/widgets/__pycache__
--- run-todo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")"
-
-.venv/bin/python src/todo.py
-
-rm -rf src/boxes_tui/__pycache__
-rm -rf src/boxes_tui/widgets/__pycache__
